fb7206cef7a66356d967bb150db71a98a644ce9d
ankur handa <ankurhanda@ankurs-MacBook-Pro.local>
2018-01-17 16:41:39 +0000

    able to compile the graph
    Signed-off-by: ankur handa <ankurhanda@ankurs-MacBook-Pro.local>

--- UNet.py
+++ UNet.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import tensorflow.contrib.layers as layers
 import resnet_utils
+import numpy as np
 
 
 class UNet(object):
 
-    def __init__(self, batch_size, img_height, img_width, learning_rate, sess):
+    def __init__(self, batch_size, img_height, img_width, learning_rate, sess, num_classes=14, is_training=True):
 
         self.input_tensor = tf.placeholder(tf.float32, [batch_size, img_height, img_width, 3])
 
@@ -15,10 +16,10 @@ class UNet(object):
         he_initializer = tf.contrib.layers.variance_scaling_initializer()
         self.prediction, self.pred_classes = self.build_network(input_tensor= self.input_tensor,
                                              initializer=he_initializer,
-                                             is_training=True,
+                                             is_training=is_training,
                                              num_classes=14)
 
-        self.flattened_pred = tf.reshape(self.prediction, [-1])
+        self.flattened_pred = tf.reshape(self.prediction, [-1, num_classes])
 
         # Define loss and optimizer
         loss_map  = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.flattened_pred, labels=self.gt_labels)
@@ -31,91 +32,110 @@ class UNet(object):
         with tf.control_dependencies(update_ops):
             self.train_op = self.optimizer.minimize(self.cost)
 
-
         self.sess = sess
 
 
     def build_network(self, input_tensor, initializer, is_training, num_classes=14):
 
-        conv_layer = layers.conv2d(input_tensor, num_outputs=64, kernel_size=(3,3),
-                                   stride=1, padding='SAME', weights_initializer=initializer,
-                                   activation_fn=tf.identity)
-        conv_layer_enc_64 = resnet_utils.conv_bn_layer(conv_layer, name='encoder', kernel_size=(3,3),
-                                                output_channels=64, initializer=initializer,
-                                                stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer = layers.max_pool2d(inputs=conv_layer_enc_64, kernel_size=(2,2), stride=2)
+        with tf.variable_scope('encoder') as scope:
 
-
-        # Input size is H/2 x W/2
-        conv_layer_enc_128 = resnet_utils.conv_bn_layer(conv_layer, name='encoder', kernel_size=(3, 3),
-                                                   output_channels=128, initializer=initializer,
-                                                   stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_enc_128 = resnet_utils.conv_bn_layer(conv_layer_enc_128, name='encoder', kernel_size=(3, 3),
-                                                   output_channels=128, initializer=initializer,
-                                                   stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer = layers.max_pool2d(inputs=conv_layer_enc_128, kernel_size=(2, 2), stride=2)
-
-
-        #Input size is H/4 x W/4
-        conv_layer_enc_256 = resnet_utils.conv_bn_layer(conv_layer, name='encoder', kernel_size=(3, 3),
-                                                    output_channels=256, initializer=initializer,
+            conv_layer = layers.conv2d(input_tensor, num_outputs=64, kernel_size=(3,3),
+                                       stride=1, padding='SAME', weights_initializer=initializer,
+                                       activation_fn=tf.identity)
+            conv_layer_enc_64 = resnet_utils.conv_bn_layer(conv_layer, kernel_size=(3,3),
+                                                    output_channels=64, initializer=initializer,
                                                     stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_enc_256 = resnet_utils.conv_bn_layer(conv_layer_enc_256, name='encoder', kernel_size=(3, 3),
-                                                    output_channels=256, initializer=initializer,
-                                                    stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer = layers.max_pool2d(inputs=conv_layer_enc_256, kernel_size=(2, 2), stride=2)
-
-        # Input size is H/8 x W/8
-        conv_layer_enc_512 = resnet_utils.conv_bn_layer(conv_layer, name='encoder', kernel_size=(3, 3),
-                                                    output_channels=512, initializer=initializer,
-                                                    stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_enc_512 = resnet_utils.conv_bn_layer(conv_layer_enc_512, name='encoder', kernel_size=(3, 3),
-                                                    output_channels=512, initializer=initializer,
-                                                    stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer = layers.max_pool2d(inputs=conv_layer_enc_512, kernel_size=(2, 2), stride=2)
+            conv_layer = layers.max_pool2d(inputs=conv_layer_enc_64, kernel_size=(2,2), stride=2)
 
 
-        conv_layer_enc_1024 = resnet_utils.conv_bn_layer(conv_layer, name='encoder', kernel_size=(3, 3),
-                                                    output_channels=1024, initializer=initializer,
-                                                    stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_dec_512 = resnet_utils.conv_bn_layer(conv_layer_enc_1024, name='dencoder', kernel_size=(3, 3),
-                                                    output_channels=512, initializer=initializer,
-                                                    stride=1, bn=True, is_training=is_training, relu=True)
+            # Input size is H/2 x W/2
+            conv_layer_enc_128 = resnet_utils.conv_bn_layer(conv_layer, kernel_size=(3, 3),
+                                                       output_channels=128, initializer=initializer,
+                                                       stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_enc_128 = resnet_utils.conv_bn_layer(conv_layer_enc_128, kernel_size=(3, 3),
+                                                       output_channels=128, initializer=initializer,
+                                                       stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer = layers.max_pool2d(inputs=conv_layer_enc_128, kernel_size=(2, 2), stride=2)
 
-        conv_layer_dec_1024=  tf.concat([conv_layer_dec_512, conv_layer_enc_512], axis=3)
-        conv_layer_dec_512 = resnet_utils.conv_bn_layer(conv_layer_dec_1024, name='dencoder', kernel_size=(3, 3),
+
+            #Input size is H/4 x W/4
+            conv_layer_enc_256 = resnet_utils.conv_bn_layer(conv_layer, kernel_size=(3, 3),
+                                                        output_channels=256, initializer=initializer,
+                                                        stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_enc_256 = resnet_utils.conv_bn_layer(conv_layer_enc_256, kernel_size=(3, 3),
+                                                        output_channels=256, initializer=initializer,
+                                                        stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer = layers.max_pool2d(inputs=conv_layer_enc_256, kernel_size=(2, 2), stride=2)
+
+            # Input size is H/8 x W/8
+            conv_layer_enc_512 = resnet_utils.conv_bn_layer(conv_layer, kernel_size=(3, 3),
                                                         output_channels=512, initializer=initializer,
                                                         stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_dec_256 = resnet_utils.conv_bn_layer(conv_layer_dec_512, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=256, initializer=initializer,
+            conv_layer_enc_512 = resnet_utils.conv_bn_layer(conv_layer_enc_512, kernel_size=(3, 3),
+                                                        output_channels=512, initializer=initializer,
                                                         stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer = layers.max_pool2d(inputs=conv_layer_enc_512, kernel_size=(2, 2), stride=2)
 
 
-        conv_layer_dec_512 = tf.concat([conv_layer_dec_256, conv_layer_enc_256], axis=3)
-        conv_layer_dec_256 = resnet_utils.conv_bn_layer(conv_layer_dec_512, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=256, initializer=initializer,
-                                                        stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_dec_128 = resnet_utils.conv_bn_layer(conv_layer_dec_256, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=128, initializer=initializer,
+            conv_layer_enc_1024 = resnet_utils.conv_bn_layer(conv_layer, kernel_size=(3, 3),
+                                                        output_channels=1024, initializer=initializer,
                                                         stride=1, bn=True, is_training=is_training, relu=True)
 
+        with tf.variable_scope('decoder') as scope:
 
-        conv_layer_dec_256 = tf.concat([conv_layer_dec_128, conv_layer_enc_128], axis=3)
-        conv_layer_dec_128 = resnet_utils.conv_bn_layer(conv_layer_dec_256, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=128, initializer=initializer,
-                                                        stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_128, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=64, initializer=initializer,
-                                                        stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_dec_512 = resnet_utils.conv_bn_layer(conv_layer_enc_1024, kernel_size=(3, 3),
+                                                            output_channels=512, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+
+            reduced_patchsize = np.multiply(conv_layer_dec_512.get_shape().as_list()[1:3], 2)
+            conv_layer_dec_512 = tf.image.resize_images(conv_layer_dec_512, size=reduced_patchsize,
+                                                            method=tf.image.ResizeMethod.BILINEAR)
+
+            conv_layer_dec_1024=  tf.concat([conv_layer_dec_512, conv_layer_enc_512], axis=3)
+            conv_layer_dec_512 = resnet_utils.conv_bn_layer(conv_layer_dec_1024, kernel_size=(3, 3),
+                                                            output_channels=512, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_dec_256 = resnet_utils.conv_bn_layer(conv_layer_dec_512, kernel_size=(3, 3),
+                                                            output_channels=256, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+
+            reduced_patchsize = np.multiply(conv_layer_dec_256.get_shape().as_list()[1:3], 2)
+            conv_layer_dec_256 = tf.image.resize_images(conv_layer_dec_256, size=reduced_patchsize,
+                                                        method=tf.image.ResizeMethod.BILINEAR)
+
+            conv_layer_dec_512 = tf.concat([conv_layer_dec_256, conv_layer_enc_256], axis=3)
+            conv_layer_dec_256 = resnet_utils.conv_bn_layer(conv_layer_dec_512, kernel_size=(3, 3),
+                                                            output_channels=256, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_dec_128 = resnet_utils.conv_bn_layer(conv_layer_dec_256, kernel_size=(3, 3),
+                                                            output_channels=128, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
 
 
-        conv_layer_dec_128 = tf.concat([conv_layer_dec_64, conv_layer_enc_64], axis=3)
-        conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_128, name='dencoder', kernel_size=(3, 3),
-                                                        output_channels=64, initializer=initializer,
-                                                        stride=1, bn=True, is_training=is_training, relu=True)
-        conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_64, name='dencoder', kernel_size=(3, 3),
-                                                       output_channels=64, initializer=initializer,
-                                                       stride=1, bn=True, is_training=is_training, relu=True)
+            reduced_patchsize = np.multiply(conv_layer_dec_128.get_shape().as_list()[1:3], 2)
+            conv_layer_dec_128 = tf.image.resize_images(conv_layer_dec_128, size=reduced_patchsize,
+                                                        method=tf.image.ResizeMethod.BILINEAR)
+
+            conv_layer_dec_256 = tf.concat([conv_layer_dec_128, conv_layer_enc_128], axis=3)
+            conv_layer_dec_128 = resnet_utils.conv_bn_layer(conv_layer_dec_256, kernel_size=(3, 3),
+                                                            output_channels=128, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_128, kernel_size=(3, 3),
+                                                            output_channels=64, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+
+
+            reduced_patchsize = np.multiply(conv_layer_dec_64.get_shape().as_list()[1:3], 2)
+            conv_layer_dec_64 = tf.image.resize_images(conv_layer_dec_64, size=reduced_patchsize,
+                                                        method=tf.image.ResizeMethod.BILINEAR)
+
+            conv_layer_dec_128 = tf.concat([conv_layer_dec_64, conv_layer_enc_64], axis=3)
+            conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_128, kernel_size=(3, 3),
+                                                            output_channels=64, initializer=initializer,
+                                                            stride=1, bn=True, is_training=is_training, relu=True)
+            conv_layer_dec_64 = resnet_utils.conv_bn_layer(conv_layer_dec_64, kernel_size=(3, 3),
+                                                           output_channels=64, initializer=initializer,
+                                                           stride=1, bn=True, is_training=is_training, relu=True)
 
         prediction = layers.conv2d(conv_layer_dec_64, num_outputs=num_classes, kernel_size=(3,3),
                                    stride=1, padding='SAME', weights_initializer=initializer,
@@ -135,9 +155,3 @@ class UNet(object):
         return self.sess.run([self.prediction, self.pred_classes], feed_dict={
             self.input_tensor: inputs
         })
-
-
-
-
-
-
--- train_u-net.py
+++ train_u-net.py
@@ -6,6 +6,8 @@ import read_sunrgbd_data
 from PIL import Image
 import argparse
 
+from UNet import UNet
+
 import numpy as np
 import matplotlib.pyplot as pl
 import matplotlib as mpl
@@ -34,25 +36,21 @@ def tile_images(img, batch_size, rows, cols, rgb):
         
 
 
-SUNRGBD_dataset = read_sunrgbd_data.dataset("SUNRGBD","/media/ankur/nnseg/sunrgbd_training.txt")
+# SUNRGBD_dataset = read_sunrgbd_data.dataset("SUNRGBD","/media/ankur/nnseg/sunrgbd_training.txt")
 
 # Parameters
 #learning_rate = 0.1
 training_iters = 200000
 display_step = 10
 
-img_width  = 320 
-img_height = 240
-
-batch_size = 20
-
 base_learning_rate=0.1
-learning_rate = tf.placeholder(tf.float32, shape=[])
+
+#learning_rate = tf.placeholder(tf.float32, shape=[])
 
 # tf Graph input
-x = tf.placeholder(tf.float32, [batch_size, img_height, img_width, 3])
-y = tf.placeholder(tf.int32, batch_size*img_width*img_height)
-y_bool = tf.placeholder(tf.int32, batch_size*img_width*img_height)
+# x = tf.placeholder(tf.float32, [batch_size, img_height, img_width, 3])
+# y = tf.placeholder(tf.int32, batch_size*img_width*img_height)
+# y_bool = tf.placeholder(tf.int32, batch_size*img_width*img_height)
 
 
 
@@ -72,7 +70,7 @@ colour_code = [(0, 0, 0),(0,0,1),(0.9137,0.3490,0.1882), (0, 0.8549, 0),
                (0.5843,0,0.9412),(0.8706,0.9451,0.0941),(1.0000,0.8078,0.8078),
                (0,0.8784,0.8980),(0.4157,0.5333,0.8000),(0.4588,0.1137,0.1608),
                (0.9412,0.1373,0.9216),(0,0.6549,0.6118),(0.9765,0.5451,0),
-               (0.8824,0.8980,0.7608)];
+               (0.8824,0.8980,0.7608)]
                
 cm = mpl.colors.ListedColormap(colour_code)
 
@@ -91,65 +89,84 @@ ax.axis('off')
 # Set whitespace to 0
 fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 fig.show()
-class_weights = [0, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1];
+class_weights = [0, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 1]
 weight_map = tf.constant(np.array(class_weights, dtype=np.float32))
 
-with tf.device("/gpu:0"):
-    # Construct model
-    pred, pred_bhwd = custom_layers_unet.unet(x, training=True)
-    gt = tf.cast(tf.reshape(y,[-1]),tf.int32)
-    # Define loss and optimizer
-    loss_map = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=gt)
-    loss_map = tf.multiply(loss_map,tf.to_float(tf.not_equal(gt,0)))
-    cost = tf.reduce_mean(loss_map)
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-    
-    # Initializing the variables
-    init = tf.global_variables_initializer()
-    
-    #total steps
-    #total_steps = 100
+config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+init = tf.global_variables_initializer()
 
-# Launch the graph
-    config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
-    epoch = 1
 
-    #config.gpu_options.allow_growth=True
-    with tf.Session(config=config) as sess:
-        sess.run(init)
+img_width  = 320
+img_height = 240
 
-        print('This has been initialised')    
-        step = 1
-        
-        while 1:
+batch_size = 20
+learning_rate = 1e-3
 
-            img, label = SUNRGBD_dataset.get_random_shuffle(batch_size)
-            #print(img.shape)
+with tf.Session(config=config) as sess:
+    sess.run(init)
 
-            #plt.show()
-            label = np.reshape(label,[-1])
-            
-            decay = np.floor((epoch - 1) / 30)
-            learningRate = base_learning_rate *  np.power(0.95, decay)
-            #learning_rate = (1/step) * 0.1
-            _,lr = sess.run([optimizer,learning_rate], feed_dict={x: img, y: label, learning_rate:learningRate})
-            loss= sess.run([cost], feed_dict={x: img,y: label})
-            #print(lmap.shape)
-            print('epoch = ', epoch, 'batch = ', step-(np.floor(5285/batch_size))*(epoch-1), 'loss = ', loss, 'learning rate =', lr)
-            step = step + 1
-            epoch = np.floor(step*batch_size/5285)+1
-            tpred, tpred_bhwd = sess.run([pred, pred_bhwd], feed_dict={x: img,y: label})
-            #print(tpred_bhwd.shape)
-            
-            best_labels = np.argmax(tpred_bhwd,axis=3)
-            #print(best_labels[1])
-            #print(best_labels.shape)
-                
-            batchImage = tile_images(best_labels,batch_size, rows, cols, 1)
-            im.set_data(np.uint8(batchImage));
-            #print('max = ',img[1].max(),'min= ', img[1].min())
-            #im.set_clim(vmin=0.0, vmax=255.0)
-            fig.show();
-            pl.pause(0.00001);
+    unet = UNet(batch_size, img_height, img_width, learning_rate, sess, num_classes=14, is_training=True)
+
+    # while True:
+
+
+
+# with tf.device("/gpu:0"):
+#     # Construct model
+#     pred, pred_bhwd = custom_layers_unet.unet(x, training=True)
+#     gt = tf.cast(tf.reshape(y,[-1]),tf.int32)
+#     # Define loss and optimizer
+#     loss_map = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=gt)
+#     loss_map = tf.multiply(loss_map,tf.to_float(tf.not_equal(gt,0)))
+#     cost = tf.reduce_mean(loss_map)
+#     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+#
+#     # Initializing the variables
+#     init = tf.global_variables_initializer()
+#
+#     #total steps
+#     #total_steps = 100
+#
+# # Launch the graph
+#     config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+#     epoch = 1
+#
+#     #config.gpu_options.allow_growth=True
+#     with tf.Session(config=config) as sess:
+#         sess.run(init)
+#
+#         print('This has been initialised')
+#         step = 1
+#
+#         while 1:
+#
+#             img, label = SUNRGBD_dataset.get_random_shuffle(batch_size)
+#             #print(img.shape)
+#
+#             #plt.show()
+#             label = np.reshape(label,[-1])
+#
+#             decay = np.floor((epoch - 1) / 30)
+#             learningRate = base_learning_rate *  np.power(0.95, decay)
+#             #learning_rate = (1/step) * 0.1
+#             _,lr = sess.run([optimizer,learning_rate], feed_dict={x: img, y: label, learning_rate:learningRate})
+#             loss= sess.run([cost], feed_dict={x: img,y: label})
+#             #print(lmap.shape)
+#             print('epoch = ', epoch, 'batch = ', step-(np.floor(5285/batch_size))*(epoch-1), 'loss = ', loss, 'learning rate =', lr)
+#             step = step + 1
+#             epoch = np.floor(step*batch_size/5285)+1
+#             tpred, tpred_bhwd = sess.run([pred, pred_bhwd], feed_dict={x: img,y: label})
+#             #print(tpred_bhwd.shape)
+#
+#             best_labels = np.argmax(tpred_bhwd,axis=3)
+#             #print(best_labels[1])
+#             #print(best_labels.shape)
+#
+#             batchImage = tile_images(best_labels,batch_size, rows, cols, 1)
+#             im.set_data(np.uint8(batchImage));
+#             #print('max = ',img[1].max(),'min= ', img[1].min())
+#             #im.set_clim(vmin=0.0, vmax=255.0)
+#             fig.show();
+#             pl.pause(0.00001);
   
     
