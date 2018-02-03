621338d3412d909441f86a97d054b627799309ab
ankur handa <ankurhanda@ankurs-MacBook-Pro.local>
2018-02-03 12:56:14 +0000

    updating and cleaning
    Signed-off-by: ankur handa <ankurhanda@ankurs-MacBook-Pro.local>

--- train_u-net_horovod.py
+++ train_u-net_horovod.py
@@ -33,15 +33,9 @@ SUNRGBD_dataset = read_sunrgbd_data.dataset("SUNRGBD",
                                             "/se3netsproject/data/multijtdata/baxter_babbling_rarm_3.5hrs_Dec14_16/postprocessmotions/motion0",
                                             img_type='depth')
 
-
 max_labels = 23
 
-
-# config.gpu_options.per_process_gpu_memory_fraction = 0.9
-# config.gpu_options.allow_growth = True
-
-
-batch_size = 20
+batch_size = 25
 learning_rate = 1e-3
 iter = 0
 
@@ -55,8 +49,6 @@ global_step = tf.contrib.framework.get_or_create_global_step()
 
 UNET = unet(batch_size, img_height, img_width, learning_rate, sess=None, num_classes=max_labels, is_training=True,
             img_type=img_type, use_horovod=True, global_step=global_step)
-
-
 
 hooks = [
         # Horovod: BroadcastGlobalVariablesHook broadcasts initial variable states
@@ -84,6 +76,14 @@ with tf.train.MonitoredTrainingSession(config=config, hooks=hooks) as mon_sess:
 
         label = np.reshape(label, [-1])
 
+        if iter <= 10:
+            UNET.set_learning_rate(learning_rate=1e-2)
+
+        elif (iter > 10 and iter <= 500):
+            UNET.set_learning_rate(learning_rate=1e-3)
+        else:
+            UNET.set_learning_rate(learning_rate=1e-4)
+
         batch_start = time.time()
         train_op, cost, pred, summary = UNET.train_batch(img, label)
         time_taken = time.time() - batch_start
@@ -93,38 +93,4 @@ with tf.train.MonitoredTrainingSession(config=config, hooks=hooks) as mon_sess:
 
         print('iter = ', iter, 'cost = ', cost, 'images/sec = ', images_per_sec, 'batch_size = ', batch_size)
 
-        # mon_sess.run(train_op, feed_dict={image: image_, label: label_})
-
-
-# with tf.Session(config=config) as sess:
-#
-#
-#     sess.run(tf.global_variables_initializer())
-#
-#     summary_writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
-#
-#     while True:
-#
-#         img, label = SUNRGBD_dataset.get_random_shuffle(batch_size)
-#         batch_labels = label
-#
-#         label = np.reshape(label, [-1])
-#
-#         if iter <= 10:
-#             UNET.set_learning_rate(learning_rate=1e-2)
-#
-#         elif (iter > 10 and iter <= 500):
-#             UNET.set_learning_rate(learning_rate=1e-3)
-#         else:
-#             UNET.set_learning_rate(learning_rate=1e-4)
-#
-#         batch_start = time.time()
-#         train_op, cost, pred, summary = UNET.train_batch(img, label)
-#         time_taken = time.time() - batch_start
-#         images_per_sec = batch_size / time_taken
-#
-#         summary_writer.add_summary(summary, iter)
-#
-#         print('iter = ', iter, 'cost = ', cost, 'imaged = ', images_per_sec)
-#
-#         iter = iter + 1
+        iter = iter + 1
