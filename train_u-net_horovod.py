83ea36c811b3559e5d01b7f21115aaa81b0924fc
ankur handa <ankurhanda@ankurs-MacBook-Pro.local>
2018-02-03 13:31:39 +0000

    update
    Signed-off-by: ankur handa <ankurhanda@ankurs-MacBook-Pro.local>

--- train_u-net_horovod.py
+++ train_u-net_horovod.py
@@ -77,10 +77,10 @@ with tf.train.MonitoredTrainingSession(config=config, hooks=hooks) as mon_sess:
         label = np.reshape(label, [-1])
 
         if iter <= 10:
-            UNET.set_learning_rate(learning_rate=1e-2 * hvd.size())
+            UNET.set_learning_rate(learning_rate=1e-1 * hvd.size())
 
         elif (iter > 10 and iter <= 500):
-            UNET.set_learning_rate(learning_rate=1e-3 * hvd.size())
+            UNET.set_learning_rate(learning_rate=1e-2 * hvd.size())
         else:
             UNET.set_learning_rate(learning_rate=1e-4 * hvd.size())
 
