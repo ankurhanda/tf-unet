4c5bfa861dc4cc9b1ccb465ca443231d1d11a71f
ankur handa <[ankurhanda@ankurs-MacBook-Pro] handa.ankur@gmail.com>
2018-02-17 08:42:37 +0000

    adding full 4M image experiment
    Signed-off-by: ankur handa <[ankurhanda@ankurs-MacBook-Pro] handa.ankur@gmail.com>

--- train_u-net_horovod.py
+++ train_u-net_horovod.py
@@ -120,7 +120,8 @@ with tf.train.MonitoredTrainingSession(config=config, hooks=hooks) as mon_sess:
         summary_writers[hvd.rank()].add_summary(summary, iter_num)
         summary_writers[hvd.rank()].flush()
 
-        print('iter = ', iter_num, 'hvd_rank = ', hvd.rank(), 'cost = ', cost, 'images/sec = ', images_per_sec, 'batch_size = ', batch_size)
+        print('iter = ', iter_num, 'hvd_rank = ', hvd.rank(), 'cost = ', cost, 'images/sec = ', images_per_sec, 'batch_size = ', batch_size,
+              'lr = ', cur_learning_rate, 'epochs = ', num_epochs)
 
         if write_images_per_sec_files:
             fileName = logs_path + 'time_gpus_{:03d}_gpuid_{:03d}_iter_{:03d}.txt'.format(hvd.size(), hvd.rank(), iter_num)
